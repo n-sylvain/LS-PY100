5b25f919ff78ca1c6ae8330ef5a2003bbd86d958
Sylvain Ni <sylvainni@msn.com>
2024-06-06 21:10:45 +0200

    Adding collection exercises

--- ls_intro_book/3_collections_iteration/exercise1_1.py
+++ ls_intro_book/3_collections_iteration/exercise1_1.py
@@ -5,3 +5,15 @@ print(my_range[6])
 
 print(range(0, 25, 3)[6])
 
+########
+
+my_str = "Launch School"
+print(my_str[4:10])
+
+# or
+
+my_str = "Launch School"
+start = my_str.find("c")
+print(my_str[start : start + 6])
+
+#######
